118d461edcfde8f6b2016a085b31180475856e23
a-Nirali <74716227+a-Nirali@users.noreply.github.com>
2020-12-14 16:04:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var bullet,wall,thickness;
 var speed,weight;
+
 function setup() {
   createCanvas(1600,400);
   speed=random(55,90)
@@ -17,18 +18,18 @@ function draw() {
   background("black");  
   if(wall.x-bullet.x < (bullet.width+wall.width)/3){
     bullet.velocityX=0
-    var deformation=0.5*weight*speed*speed/22500;
-    if (deformation>180)
+    var damage=0.5*weight*speed*speed/22500;
+    if (damage>180)
     {
       bullet.shapeColor=color(255,0,0);
     
     }
-    if (deformation>180 && deformation>100)
+    if (damage>180 && damage>100)
     {
       bullet.shapeColor=color(230,230,0);
     
     }
-    if (deformation<100)
+    if (damage<100)
     {
       bullet.shapeColor=color(0,255,0);
     
