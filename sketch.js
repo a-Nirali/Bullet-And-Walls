8401280c7b84559b0b297e06054f4f2dbf147e7a
a-Nirali <74716227+a-Nirali@users.noreply.github.com>
2021-01-08 10:50:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,24 +16,7 @@ weight=random(30,52)
 
 function draw() {
   background("black");  
-  if(wall.x-bullet.x < (bullet.width+wall.width)/3){
-    bullet.velocityX=0
-    var damage=0.5*weight*speed*speed/22500;
-    if (damage>180)
-    {
-      bullet.shapeColor=color(255,0,0);
-    
-    }
-    if (damage>180 && damage>100)
-    {
-      bullet.shapeColor=color(230,230,0);
-    
-    }
-    if (damage<100)
-    {
-      bullet.shapeColor=color(0,255,0);
-    
-    }
+ 
     if (hasCollided(bullet,wall))
     {
       bullet.velocityX=0;
